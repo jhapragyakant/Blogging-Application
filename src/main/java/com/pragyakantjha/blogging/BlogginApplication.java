a1947a224850047fa0c96d2cd6273c6e85a27984
Pragya <jhapragyakant@gmail.com>
2023-08-06 18:51:48 +0530

    Initial commit

---
+++ src/main/java/com/pragyakantjha/blogging/BlogginApplication.java
@@ -0,0 +1,13 @@
+package com.pragyakantjha.blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragyakantjha/blogging/BlogginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragyakantjha.blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
