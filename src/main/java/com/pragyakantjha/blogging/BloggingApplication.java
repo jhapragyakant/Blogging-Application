5e084bfd22fe97b98a19786619a3e21ee01a5012
Pragya <jhapragyakant@gmail.com>
2023-08-06 20:01:54 +0530

    Name change from "BlogginApplication" to "BloggingApplication"

--- src/main/java/com/pragyakantjha/blogging/BloggingApplication.java
+++ src/main/java/com/pragyakantjha/blogging/BloggingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogginApplication {
+public class BloggingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogginApplication.class, args);
+		SpringApplication.run(BloggingApplication.class, args);
 	}
 
 }
