bad63e9925d764d6afb8166dee65ac4275b423a0
Pragya <jhapragyakant@gmail.com>
2023-08-07 15:32:59 +0530

    Completed User Service implimentation, created user basic CRUD apis, Created ResorceNotFound Exception and added ApiResponse

--- src/main/java/com/pragyakantjha/blogging/services/impl/UserServiceImpl.java
+++ src/main/java/com/pragyakantjha/blogging/services/impl/UserServiceImpl.java
@@ -1,23 +1,22 @@
 package com.pragyakantjha.blogging.services.impl;
 
 import com.pragyakantjha.blogging.entities.User;
+import com.pragyakantjha.blogging.exceptions.ResourceNotFoundException;
 import com.pragyakantjha.blogging.payload.UserDto;
 import com.pragyakantjha.blogging.repositories.UserRepository;
 import com.pragyakantjha.blogging.services.UserService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private ModelMapper modelMapper;
+
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -28,30 +27,59 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto userDto, Integer userId) {
-        return null;
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setAbout(userDto.getPassword());
+        User updatedUser = userRepository.save(user);
+        return convertToDto(updatedUser);
     }
 
     @Override
     public UserDto getUserById(Integer userId) {
-        return null;
+        User user = userRepository.findById(userId).
+                orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
+        return convertToDto(user);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return null;
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = users.stream().map(user -> convertToDto(user)).collect(Collectors.toList());
+        return userDtos;
     }
 
     @Override
     public void deleteUser(Integer userId) {
-
+        User user = userRepository.findById(userId).
+                orElseThrow(() -> new ResourceNotFoundException("User","Id",userId));
+        userRepository.delete(user);
     }
 
     public UserDto convertToDto(User user) {
-        return modelMapper.map(user, UserDto.class);
+        UserDto userDto = new UserDto();
+
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPassword(user.getPassword());
+        userDto.setAbout(user.getAbout());
+
+        return userDto;
     }
 
     public User convertToUser(UserDto userDto) {
-        return modelMapper.map(userDto, User.class);
+        User user = new User();
+
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setAbout(userDto.getAbout());
+
+        return user;
     }
 
 }
