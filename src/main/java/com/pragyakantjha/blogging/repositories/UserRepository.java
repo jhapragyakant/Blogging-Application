c9f7daebf0f7bb7d7d913f2dc6b07ef9adcaecb0
Pragya <jhapragyakant@gmail.com>
2023-08-13 12:19:05 +0530

    added custom FindByUser and FindByCategory in post repository.

--- src/main/java/com/pragyakantjha/blogging/repositories/UserRepository.java
+++ src/main/java/com/pragyakantjha/blogging/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.pragyakantjha.blogging.repositories;
 
+import com.pragyakantjha.blogging.entities.Category;
+import com.pragyakantjha.blogging.entities.Post;
 import com.pragyakantjha.blogging.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    List<Post> findByUser(User user);
+    List<Post> findByCategory(Category category);
 }
