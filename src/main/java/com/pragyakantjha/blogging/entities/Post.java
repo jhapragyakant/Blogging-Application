5f68109c46621ae516384745ad14253407b34ce4
Pragya <jhapragyakant@gmail.com>
2023-08-13 12:20:11 +0530

    added all args and no args constructor annotation in post entity.

--- src/main/java/com/pragyakantjha/blogging/entities/Post.java
+++ src/main/java/com/pragyakantjha/blogging/entities/Post.java
@@ -1,6 +1,8 @@
 package com.pragyakantjha.blogging.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.Date;
 @Entity
 @Data
 @Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
