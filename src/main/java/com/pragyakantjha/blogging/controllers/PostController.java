95be27b49241cc81f0b5c08d03bba897213b582a
Pragya <jhapragyakant@gmail.com>
2023-08-14 17:24:29 +0530

    Added get mappings of getPostByCategory and getPostByUser methods in PostController.

--- src/main/java/com/pragyakantjha/blogging/controllers/PostController.java
+++ src/main/java/com/pragyakantjha/blogging/controllers/PostController.java
@@ -1,11 +1,14 @@
 package com.pragyakantjha.blogging.controllers;
 
+import com.pragyakantjha.blogging.entities.Post;
 import com.pragyakantjha.blogging.payload.PostDto;
 import com.pragyakantjha.blogging.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
@@ -22,4 +25,16 @@ public class PostController {
         PostDto createdPost = this.postService.createPost(postDto, userId,categoryId);
         return new ResponseEntity<PostDto>(createdPost, HttpStatus.CREATED);
     }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId){
+        List<PostDto> postDtos = this.postService.getPostByUser(userId);
+        return new ResponseEntity<>(postDtos,HttpStatus.OK);
+    }
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId){
+        List<PostDto> postDtos = this.postService.getPostByCategory(categoryId);
+        return new ResponseEntity<>(postDtos,HttpStatus.OK);
+    }
+
 }
