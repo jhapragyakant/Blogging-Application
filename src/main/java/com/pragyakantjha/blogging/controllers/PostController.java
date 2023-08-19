58b7ec1ebbefa282f24fb0d52273afda6370fe77
Pragya <jhapragyakant@gmail.com>
2023-08-19 16:56:28 +0530

    Added app constants class in project.

--- src/main/java/com/pragyakantjha/blogging/controllers/PostController.java
+++ src/main/java/com/pragyakantjha/blogging/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.pragyakantjha.blogging.controllers;
 
+import com.pragyakantjha.blogging.config.AppConstants;
 import com.pragyakantjha.blogging.payload.PostDto;
 import com.pragyakantjha.blogging.services.PostService;
 import com.pragyakantjha.blogging.utils.ApiResponse;
@@ -44,10 +45,10 @@ public class PostController {
     }
     @GetMapping("/get_all")
     public ResponseEntity<PostResponse> getAllPosts(
-            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "3", required = false) Integer pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false)String sortDir
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false)String sortDir
     ){
         PostResponse postResponse = this.postService.getAllPosts(pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity<>(postResponse,HttpStatus.OK);
