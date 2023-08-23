b5db69da51dc7bca53dfb7732e6ce6676956eefc
Pragya <jhapragyakant@gmail.com>
2023-08-23 08:47:25 +0530

    added role based authentication system.

--- src/main/java/com/pragyakantjha/blogging/controllers/UserController.java
+++ src/main/java/com/pragyakantjha/blogging/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.pragyakantjha.blogging.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -35,6 +36,7 @@ public class UserController {
     }
 
     //DELETE - delete user
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer id){
         userService.deleteUser(id);
